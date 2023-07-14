8937850c0a60507c7fc085bb9dae30a3347b913e
Paulo <paulinhoguaruja@hotmail.com>
2023-07-14 17:59:36 -0300

    manipulando o evento da lista (LI)

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,10 @@
 const personagens = document.querySelectorAll(".personagem");
 console.log(personagens);
+
+personagens.forEach(personagem => {
+    personagem.addEventListener("mouseenter", () => {
+        const personagemSelecionado = document.querySelector(".selecionado");
+        personagemSelecionado.classList.remove("selecionado");
+        personagem.classList.add("selecionado");
+    } )
+})
